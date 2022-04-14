eafbf624afeeb5d541f8795352cd68607eea1e25
Alin Gavriliu <alin_gavriliu@yahoo.com>
2022-04-14 23:44:59 +0300

    Added registration form in sign in page

--- pages/login/app.js
+++ pages/login/app.js
@@ -1,4 +1,6 @@
 const inputs = document.querySelectorAll(".input-field");
+const toggleButtons = document.querySelectorAll(".toggle");
+const main = document.querySelector("main");
 
 inputs.forEach((input) => {
   input.addEventListener("focus", () => {
@@ -7,5 +9,11 @@ inputs.forEach((input) => {
   input.addEventListener("blur", () => {
     if (input.value !== "") return;
     input.classList.remove("active");
+  });
+});
+
+toggleButtons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    main.classList.toggle("sign-up-mode");
   });
 });
