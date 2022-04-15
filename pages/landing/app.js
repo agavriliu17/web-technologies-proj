4a1bbf2007c4697f871d7075fc0df2991145eff9
Alin Gavriliu <alin_gavriliu@yahoo.com>
2022-04-15 09:51:16 +0300

    Made login responsive & footer wave improvements

--- pages/landing/app.js
+++ pages/landing/app.js
@@ -10,10 +10,23 @@ const main = document.querySelector("main");
 
 declare();
 
+//TODO: Implement for all sections
+const scrollInto = () => {
+  var scrollDiv = document
+    .getElementById("about-section")
+    .getBoundingClientRect();
+
+  window.scrollTo({ top: scrollDiv.top, behavior: "smooth" });
+
+  // document.getElementById("footer").scrollIntoView(true);
+};
+
 function events() {
   hamburgerMenu.addEventListener("click", () => {
     mainWrapper.classList.toggle("active");
   });
+
+  document.getElementById("about-us").addEventListener("click", scrollInto);
 }
 
 events();
