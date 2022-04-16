22e2f9f6344b408f355acfd5f1839f62e3be0ba5
Rianer <75773955+Rianer@users.noreply.github.com>
2022-04-16 16:26:55 +0300

    [Home page] Added list view + improved design

--- pages/homepage/app.js
+++ pages/homepage/app.js
@@ -5,6 +5,12 @@ const main = document.querySelector("main");
 const icon = document.querySelector('.search-icon');
 const search = document.querySelector('.search-bar');
 
+const item_list = document.querySelector(".item-list");
+const grid_btn = document.getElementById("show-grid");
+const list_btn = document.getElementById("show-details");
+
+grid_btn.classList.add('active-btn');
+
 function declare() {
   mainWrapper = document.querySelector(".main-wrapper");
   hamburgerMenu = document.querySelector(".hamburger-menu");
@@ -14,6 +20,18 @@ function events() {
   hamburgerMenu.addEventListener("click", () => {
     mainWrapper.classList.toggle("active");
   });
+
+  grid_btn.addEventListener("click", () => {
+  	grid_btn.classList.add("active-btn");
+  	list_btn.classList.remove('active-btn');
+  	item_list.classList.remove('list-view');
+  })
+
+  list_btn.addEventListener("click", () => {
+  	list_btn.classList.add("active-btn");
+  	grid_btn.classList.remove('active-btn');
+  	item_list.classList.add('list-view');
+  })
 }
 
 
