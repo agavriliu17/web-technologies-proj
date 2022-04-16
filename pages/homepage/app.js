7b1527a9db571cc111889c4364c035a2105d0628
Rianer <75773955+Rianer@users.noreply.github.com>
2022-04-16 12:54:09 +0300

    Home fixes

--- pages/homepage/app.js
+++ pages/homepage/app.js
@@ -4,3 +4,18 @@ const search = document.querySelector('.search-bar');
 icon.onclick = function(){
 	search.classList.toggle('active');
 }
+
+function declare(){
+	var hamburgerMenu = document.querySelector(".hamburger-menu");
+	var mainWrapper = document.querySelector(".main-wrapper");
+}
+
+declare();
+
+function events() {
+  hamburgerMenu.addEventListener("click", () => {
+    mainWrapper.classList.toggle("responsive");
+  });
+}
+
+events();
