5b57581cea9ced6b32d06af734bc33359107cec0
Alin Gavriliu <alin_gavriliu@yahoo.com>
2022-06-21 01:00:12 +0300

    Changed project structure & auth methods init

---
+++ backend/controllers/userController.js
@@ -0,0 +1,153 @@
+const bcrypt = require("bcrypt");
+
+const User = require("../models/userModel");
+const { getPostData } = require("../utils");
+
+// @desc Get all users
+// @route GET api/v1/users
+async function getUsers(req, res) {
+  try {
+    const users = await User.findAll();
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(users));
+  } catch (error) {
+    console.log(error);
+    res.writeHead(500, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "Something went wrong" }));
+  }
+}
+
+async function postUser(req, res) {
+  try {
+    let body = await getPostData(req);
+    const { name, nickname, password, email } = JSON.parse(body);
+    const user = {
+      name,
+      nickname,
+      password,
+      email,
+    };
+    let newUser = await User.insertUser(user);
+    if (newUser == "invalid data") {
+      res.writeHead(409, { "Content-Type": "application/json" });
+      return res.end(
+        JSON.stringify({ message: "User with this data already exists" })
+      );
+    }
+    if (newUser == null) {
+      res.writeHead(400, { "Content-Type": "application/json" });
+      return res.end(
+        JSON.stringify({ message: "Something went wrong: User not added" })
+      );
+    }
+    res.writeHead(201, { "Content-Type": "application/json" });
+    return res.end(JSON.stringify(newUser));
+  } catch (error) {
+    console.log(error);
+    res.writeHead(500, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "Something went wrong" }));
+  }
+}
+
+async function getUserByNickname(req, res, nickname) {
+  try {
+    const user = await User.findUserByNickname(nickname);
+    if (!user) {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "User Not Found" }));
+    } else {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(user));
+    }
+  } catch (error) {
+    console.log(error);
+    res.writeHead(500, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "Something went wrong" }));
+  }
+}
+
+async function getUserById(req, res, id) {
+  try {
+    const user = await User.findUserById(id);
+    if (!user) {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "User Not Found" }));
+    } else if (user == "invalid format") {
+      res.writeHead(400, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "Wrong Id Format" }));
+    } else {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(user));
+    }
+  } catch (error) {
+    console.log(error);
+    res.writeHead(500, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "Something went wrong" }));
+  }
+}
+
+async function getUserByEmail(req, res, email) {
+  try {
+    const user = await User.findUserByEmail(email);
+    if (!user) {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "User Not Found" }));
+    } else {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(user));
+    }
+  } catch (error) {
+    console.log(error);
+    res.writeHead(500, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "Something went wrong" }));
+  }
+}
+
+//Auth routes
+async function registerUser(req, res) {
+  try {
+    let body = await getPostData(req);
+    const { name, nickname, password, email } = JSON.parse(body);
+    const user = {
+      name,
+      nickname,
+      password,
+      email,
+    };
+
+    const saltRounds = 10;
+    const encryptedPassword = await bcrypt.hash(password, saltRounds);
+    const newUser = await User.insertUser({
+      ...user,
+      password: encryptedPassword,
+    });
+
+    if (newUser === "invalid data") {
+      res.writeHead(409, { "Content-Type": "application/json" });
+      return res.end(
+        JSON.stringify({ message: "User with this data already exists" })
+      );
+    }
+    if (newUser === null) {
+      res.writeHead(400, { "Content-Type": "application/json" });
+      return res.end(
+        JSON.stringify({ message: "Something went wrong: User not added" })
+      );
+    }
+    res.writeHead(201, { "Content-Type": "application/json" });
+    return res.end(JSON.stringify(newUser));
+  } catch (error) {
+    console.log(error);
+    res.writeHead(500, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "Something went wrong" }));
+  }
+}
+
+module.exports = {
+  getUsers,
+  postUser,
+  getUserByNickname,
+  getUserById,
+  getUserByEmail,
+  registerUser,
+};
---
+++ backend/server.js
@@ -0,0 +1,69 @@
+const http = require("http");
+const {
+  getUsers,
+  postUser,
+  getUserByNickname,
+  getUserById,
+  getUserByEmail,
+  registerUser,
+} = require("./controllers/userController");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/welcome") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "Welcome" }));
+  }
+
+  //Get all users, path: /api/v1/users
+  else if (req.url === "/api/v1/users" && req.method === "GET") {
+    getUsers(req, res);
+  }
+
+  //Add user, path: /api/v1/users
+  else if (req.url === "/api/v1/users" && req.method === "POST") {
+    postUser(req, res);
+  }
+
+  //Create user, path: /api/v1/register
+  else if (req.url === "/api/v1/register" && req.method === "POST") {
+    registerUser(req, res);
+  }
+
+  //Get user by id, path: /api/v1/users/{id}
+  else if (
+    req.url.match(/\/api\/v1\/users\/([0-9a-f\-]+$)/) &&
+    req.method === "GET"
+  ) {
+    let id = req.url.split("/")[4];
+    getUserById(req, res, id);
+  }
+
+  //Get user by nickname, path: /api/v1/users/nickname={nickname}
+  else if (
+    req.url.match(/\/api\/v1\/users\/nickname=([a-zA-Z0-9_.\(\)]+$)/) &&
+    req.method === "GET"
+  ) {
+    let info = req.url.split("/")[4];
+    let nickname = info.split("=")[1];
+    getUserByNickname(req, res, nickname);
+  }
+
+  //Get user by email, path: /api/v1/users/email={email}
+  else if (
+    req.url.match(
+      /\/api\/v1\/users\/email=(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    ) &&
+    req.method === "GET"
+  ) {
+    let info = req.url.split("/")[4];
+    let email = info.split("=")[1];
+    getUserByEmail(req, res, email);
+  } else {
+    res.writeHead(400, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "Unknown Request" }));
+  }
+});
+
+const PORT = process.env.PORT || 3010;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
